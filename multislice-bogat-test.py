30aca4783f781adaa6217e9bee4cc2c8256c4b4c
Tiger2lala <Msolo.tzhang@gmail.com>
2024-11-15 15:19:31 +0000

    added gradient delay correction. full example of bogat MB

--- multislice-bogat-test.py
+++ multislice-bogat-test.py
@@ -1,12 +1,12 @@
 import numpy as np
-from yaptxd.utils import sinc_pulse
+from yaptxd.utils import sinc_pulse, gradient_delay_adjust
 from yaptxd.spokes import SpokesForm
 from yaptxd.maps import FieldMap
 from yaptxd.sta import StaOpt
 from yaptxd.gradient_opt import BOGAT
 from yaptxd.io import write_spokes_xy_moment, write_spokes_coeff
 
-sinc2 = sinc_pulse(256, 2)
+sinc2 = sinc_pulse(147, 2)
 spokes = SpokesForm(2)
 
 spokes.set_subpulse(sinc2, 2.0)
@@ -15,8 +15,8 @@ spokes.set_ksamples(np.array([[0,1], [0,0]])*10)
 
 # spokes.plot_pulse()
 
-m = FieldMap('demo-data/vol-acpc4-mat/AdjDataUser.mat')
-m.group_slices(2,2)
+m = FieldMap('demo-data/vol-8-sli/AdjDataUser.mat')
+m.group_slices(2,4)
 
 bogat_solver = BOGAT(m, spokes)
 bogat_solver.set_rf_kwargs(tikhonov=1e-2)
@@ -34,6 +34,11 @@ print(coeff_sol)
 
 for ipulse in range(len(coeff_sol)):
     for iMB in range(len(coeff_sol[0])):
-        pulse_id = iMB + ipulse * len(coeff_sol[0])
-        write_spokes_coeff(f'{pulse_id}.txt', coeff_sol[ipulse][iMB])
+        pulse_id = ipulse + iMB * len(coeff_sol)
+        coeff = gradient_delay_adjust(coeff_sol[ipulse][iMB], 
+                                  m.z[pulse_id], 
+                                  np.max(spokes.g[:,2]), 
+                                  np.array([0,0,-10]), 
+                                  np.array([0,0,10]))
+        write_spokes_coeff(f'{pulse_id}.txt', coeff)
         write_spokes_xy_moment(f'{pulse_id}-xy.txt', bogat_solver.pulse_form_best[ipulse])
--- yaptxd/utils.py
+++ yaptxd/utils.py
@@ -8,7 +8,7 @@ import numpy as np
 from typing import Tuple
 
 GAMMA = 42.58e6  # Hz/T
-SLEW = 40 # T/m/s
+SLEW = 200 # T/m/s
 
 def sinc_pulse(nt: int, tbw: float) -> np.ndarray:
     t = np.linspace(-tbw/2, tbw/2, nt)
@@ -32,3 +32,33 @@ def trapz_gradient(nt_plat: int, plat_amp: float, slew_per_step: float,
 
     t_ramp = n_ramp-1
     return (grad_out * (-1 if rev_polarity else 1), t_ramp)
+
+
+
+def gradient_delay_adjust(coeff: np.ndarray, z: float, gz: float,
+                          delay: np.ndarray, eddy_current: np.ndarray,
+                          n_coils: int = 8) -> np.ndarray:
+    """
+    Adjust phases for pulses to account for gradient delays.
+    Only works in the slice direction for now.
+
+    :param coeff: Coefficients of 1 spokes pulse.
+    :param z: Slice position in m.
+    :param gz: Slice selection gradient in T/m.
+    :param delay: Gradient delays [R, P, S] in us.
+    :param eddy_current: Eddy current phase [R, P, S] in degrees.
+    """
+
+    if np.any(delay[0:2]):
+        raise NotImplementedError("Only slice direction is supported.")
+    
+    ccoeff = coeff.reshape(-1, n_coils).copy() # (nPulse, nCoils)
+
+    phase_add = np.ones(ccoeff.shape[0], dtype=np.complex128)
+    phase_even = eddy_current[2] * np.pi / 180\
+        + 2 * gz * z * delay[2] * 1e-6 * GAMMA * 2 * np.pi
+
+    phase_add[1::2] = phase_add[1::2] * np.exp(1j * phase_even)
+    ccoeff *= phase_add[..., np.newaxis]
+
+    return ccoeff.reshape(-1)
